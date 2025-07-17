6fab3c86b8f1aa0393885d18150a4833ff468f32
yurihp01 <yurihp01@hotmail.com>
2025-07-17 12:39:35 +0100

    Initial Commit

---
+++ DeviceInfo/DeviceInfoApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeviceInfoApp.swift
+//  DeviceInfo
+//
+//  Created by Yuri on 17/07/25.
+//
+
+import SwiftUI
+
+@main
+struct DeviceInfoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
